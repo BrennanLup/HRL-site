9d1e37af5cbd621891bf3ec7850b4136c5adf846
BrennanLup <64103046+BrennanLup@users.noreply.github.com>
2020-08-04 22:07:52 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,17 @@
-// Nav Bar
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li")
+//Why-HRL
+// FAQ code
 
-  burger.addEventListener('click', () => {
-    // fixed the body
-    document.querySelector("body").classList.toggle('body-fixed')
-    // toggle Nav
-    nav.classList.toggle('nav-active');
-    //Animate Links
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = ''
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .5}s`;
-      }
-    });
-    //burger animation
-    burger.classList.toggle('toggle');
-
-    //if window is reszied
-    window.addEventListener('resize', function(event) {
-      document.querySelector("body").classList.toggle('body-fixed')
-      nav.classList.toggle('nav-active');
-    })
-
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
   });
-
-
-
-
 }
-navSlide();
